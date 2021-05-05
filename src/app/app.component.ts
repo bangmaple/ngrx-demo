fb62267e3d1475ec952adf04e2c991afc3f1e270
bangmaple <webcam1362000@gmail.com>
2021-05-06 00:14:22 +0700

    First commit with simple and post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Observable} from 'rxjs';
+import {Store} from '@ngrx/store';
+import {Post} from '../models/posts.model';
+import * as PostActions from '../actions/post.action';
+
+
+interface AppState {
+  post: Post;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +15,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'untitled';
+  post$: Observable<Post>;
+  text: string;
+
+  constructor(private store: Store<AppState>) {
+    this.post$ = this.store.select('post');
+  }
+
+  editText() {
+    this.store.dispatch(new PostActions.EditText(this.text));
+  }
+
+  resetPost() {
+    this.store.dispatch(new PostActions.Reset());
+  }
+
+  upvote() {
+    this.store.dispatch(new PostActions.Upvote());
+  }
+
+  downvote() {
+    this.store.dispatch(new PostActions.Downvote());
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {StoreModule} from '@ngrx/store';
+import {simpleReducer} from '../reducers/simple.reducer';
+import {FormsModule} from '@angular/forms';
+import {postReducer} from '../reducers/post.reducer';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot(
+      {message: simpleReducer, post: postReducer}),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
